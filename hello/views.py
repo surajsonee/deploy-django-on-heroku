eb0b59bf847828cba9c9726fdd797730040f0abb
Perfect <suraj1.weavebytes@gmail.com>
2016-05-10 17:39:21 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -17,13 +17,4 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-    
-def home(request):
-	news = Registration.objects.all()
-	newsdetail = {
-		"newsinfo" : news
-	}
-	print newsdetail
-	return render_to_response('index.html',
-	newsdetail, context_instance=RequestContext(request))
 
